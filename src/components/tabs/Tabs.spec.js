759fd13895e518e001a30c5d825ad0226c6fe357
Antério Vieira <anteriovieira@gmail.com>
2018-02-23 16:44:23 -0300

    start tests

--- src/components/tabs/Tabs.spec.js
+++ src/components/tabs/Tabs.spec.js
@@ -1,10 +1,24 @@
 import { mount } from '@vue/test-utils'
 import BTabs from '@components/tabs/Tabs'
+import BTabItem from '@components/tabs/TabItem'
 
 describe('BTabs', () => {
     it('is called', () => {
         const wrapper = mount(BTabs)
         expect(wrapper.name()).toBe('BTabs')
         expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+
+    it('should register tabs', () => {
+        const wrapper = mount(BTabs, {
+            slots: {
+                default: [BTabItem, BTabItem]
+            }
+        })
+
+        const tabItem = wrapper.find(BTabItem)
+        expect(wrapper.vm.tabItems.length).toBe(2)
+        tabItem.destroy()
+        expect(wrapper.vm.tabItems.length).toBe(1)
     })
 })
